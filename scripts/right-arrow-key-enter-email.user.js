1433681434bad31e12e21d8da5bccda3c9ea41d0
Steven Wang <git@silverwzw.com>
2021-12-05 01:17:29 -0500

    Update right-arrow-key-enter-email.user.js

--- scripts/right-arrow-key-enter-email.user.js
+++ scripts/right-arrow-key-enter-email.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Enter email through right arrow key
+// @name         Enter id through right arrow key
 // @namespace    http://silverwzw.com/
 // @version      0.1
 // @author       Silverwzw
@@ -7,17 +7,20 @@
 // @exclude      /^https?://[^/]+\.google.com\/.*/
 // @exclude      /^https?://[^/]+\.gmail.com\/.*/
 // @exclude      /^https?://[^/]+\.youtube.com\/.*/
-// @grant        none
+// @grant        unsafeWindow
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    const debug = false;
+    if (!("userscript_debug" in unsafeWindow)) {
+        unsafeWindow.userscript_debug = false;
+    }
+
     const placeholder_delay = 500;
-    // const suffix = "@your_domain.com";
-    // const tag_user = "user_name";
-    const supported_input_types = new Set(["text", "email", "url"]);
+    const suffix = "@your_domain.com";
+    const tag_user = "user_name";
+    const supported_input_types = new Set(["text", "id", "url", "email"]);
     const supported_protocols = new Set(["http", "https"]);
     const regional_domains = new Set(["cn", "us", "tw", "hk", "ca", "au", "uk"]);
     const wellknown_domains = new Set(["com", "gov", "edu", "org"]);
@@ -27,15 +30,21 @@
         {re: /alipay\.com$/, prefix: "ali"},
         {re: /bilibili\.com$/, tag: "bilibili.com"},
         {re: /retiehe\.com$/, tag: "retiehe.com"},
+        {re: /nas\.silverwzw\.com$/, id: "Silverwzw"},
+        {re: /labstack\.com$/, disable: true }
     ];
 
     const url = new URL(document.URL);
     if (!supported_protocols.has(url.protocol.slice(0, -1).toLowerCase())) {
         // disable the logic on local html file
-        log(debug, `Disabled due to incorrect protocol ${url.protocol}.`);
+        log(unsafeWindow.userscript_debug, `Disabled due to incorrect protocol ${url.protocol}.`);
         return;
     }
-    const email = resolve(url.hostname.toLowerCase());
+    const disabled_domain = Symbol("disabled_domain");
+    const id = resolve(url.hostname.toLowerCase());
+    if (id === disabled_domain) {
+        return;
+    }
 
     function log(should_log, ...rest) {
         if (!should_log) {
@@ -45,14 +54,17 @@
     }
 
     function resolve(hostname) {
-        for (const {re, email, prefix, tag} of hardcode_hostnames) {
+        for (const {re, id, prefix, tag, disable} of hardcode_hostnames) {
             if (hostname.match(re)) {
-                if (email !== undefined) {
-                    return email;
+                if (id !== undefined) {
+                    return id;
                 } else if (prefix !== undefined) {
                     return prefix + suffix;
                 } else if (tag !== undefined) {
                     return `${tag_user}+A.${tag}@gmail.com`;
+                } else if (disable) {
+                    log(unsafeWindow.userscript_debug, `Disabled due to '${hostname}' is a disabled hostname.`);
+                    return disabled_domain;
                 }
             }
         }
@@ -81,7 +93,7 @@
         switch (nodeName.toUpperCase()) {
           case "INPUT":
             if (!supported_input_types.has(type)) {
-                log(debug, `Invalid input type: "${type}" }.`);
+                log(debug, `Invalid input type: "${type}".`);
                 return false;
             } break;
           case "TEXTAREA":
@@ -96,15 +108,15 @@
             return false;
         }
 
-        if (typeof maxLength === "number" && maxLength >= 0 && email.length > maxLength) {
-            log(debug, `Disabled due to max-length = ${maxLength}, email = ${email}.`);
+        if (typeof maxLength === "number" && maxLength >= 0 && id.length > maxLength) {
+            log(debug, `Disabled due to max-length = ${maxLength}, id = ${id}.`);
             return false;
         }
-        if (typeof minLength === "number" && minLength >= 0 && email.length < minLength) {
-            log(debug, `Disabled due to min-length = ${minLength}, email = ${email}.`);
+        if (typeof minLength === "number" && minLength >= 0 && id.length < minLength) {
+            log(debug, `Disabled due to min-length = ${minLength}, id = ${id}.`);
             return false;
         }
-        if (typeof pattern === "string" && pattern.length !== 0 && !email.match(pattern)) {
+        if (typeof pattern === "string" && pattern.length !== 0 && !id.match(pattern)) {
             log(debug, `Disabled due to pattern = ${pattern}.`);
             return false;
         }
@@ -116,10 +128,10 @@
         if (event.key !== "ArrowRight") {
             return;
         }
-        if (!isTargetValid(event.target, debug)) {
+        if (!isTargetValid(event.target, unsafeWindow.userscript_debug)) {
             return;
         }
-        event.target.value = email;
+        event.target.value = id;
         event.preventDefault();
     }
 
@@ -133,10 +145,10 @@
             return;
         }
 
-        target.placeholder = email;
+        target.placeholder = id;
 
         target.addEventListener("blur", event => {
-            if (target.placeholder === email) {
+            if (target.placeholder === id) {
                 target.placeholder = "";
             }
         }, { once: true, passive: true });
